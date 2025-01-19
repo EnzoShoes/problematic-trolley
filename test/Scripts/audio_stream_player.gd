16f403e538401cca3810b06ccac10244300093a7
Drelexal <153316184+Drelexal@users.noreply.github.com>
2025-01-19 20:16:19 +0100

    feat: imported sprites into the main scene

--- test/Scripts/audio_stream_player.gd
+++ test/Scripts/audio_stream_player.gd
@@ -8,7 +8,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
 	if playing == false:
 		_set_playing(true)
 	else:
