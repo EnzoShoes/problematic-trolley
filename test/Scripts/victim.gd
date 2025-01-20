727eee9323ef7d8ecf63d1799c741291e0f5495e
Eagleast <julesdugon@gmail.com>
2025-01-20 16:57:15 +0100

    fix(victim) : cleaned the code and added comments

--- test/Scripts/victim.gd
+++ test/Scripts/victim.gd
@@ -1,4 +1,5 @@
 extends Node2D
+
 @onready var area_2d: Area2D = $Area2D
 @onready var upper_body_sprite: Sprite2D = $dead_limbs/upper_body/Sprite2D
 @onready var lower_body_sprite: Sprite2D = $dead_limbs/lower_body/Sprite2D
@@ -6,50 +7,41 @@ extends Node2D
 @onready var upper_body: RigidBody2D = $dead_limbs/upper_body
 @onready var lower_body: RigidBody2D = $dead_limbs/lower_body
 @onready var blood_explosion: AnimatedSprite2D = $blood_explosion
-@onready var blood_1: Sprite2D = $Node2D/blood_1
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 @export var ressource: Resource
+
 var explosion_force: int = 100
 var explosion_randomness: float = 0.5 #0 = no random ; 1 = full random
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
+	# loading the texture from the chosen ressource file because many resources could be used, this script is the same for all types of victims
 	upper_body_sprite.texture = ressource.sprite_dead_upper
 	lower_body_sprite.texture = ressource.sprite_dead_lower
 	sprite_2d.texture = ressource.sprite_alive
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	#if Input.is_action_just_pressed("ui_accept"):
-		#death()
-
-	pass
 
 func death():
-	sprite_2d.visible = false
-	lower_body.visible = true
+	sprite_2d.visible = false #makes the main sprite go away because the character is dead and we want to display the dead sprite
+	lower_body.visible = true #displays the dead sprites
 	upper_body.visible = true
-	# I want to add randomness to the impulse so it is more realistic but cant manage to do it 
+	
+	#create the impulse and adds randomness to it
 	lower_body.apply_impulse((Vector2.DOWN +Vector2(randf_range(-explosion_randomness,explosion_randomness),0)) * explosion_force, Vector2(randf_range(-explosion_randomness,explosion_randomness), randf_range(-explosion_randomness,explosion_randomness)))
-
 	upper_body.apply_impulse((Vector2.UP +Vector2(randf_range(-explosion_randomness,explosion_randomness),0)) * explosion_force, Vector2(randf_range(-explosion_randomness,explosion_randomness), randf_range(-explosion_randomness,explosion_randomness)))
 
+	# victim is dead so we want blood to erupt from it
 	blood_explosion.visible = true
 	blood_explosion.play("blood_explosion")
-	
+	#we want to make a stain of blood appear, so its not too repetitive there is a random selection
 	var chosen_blood : int = randi_range(1,4)
-	print(chosen_blood)
 	var chosen_blood_node = get_node("Node2D/blood_"+ str(chosen_blood))
 	chosen_blood_node.visible = true
 	
+	# plays the sound of the death of the victim
 	if audio_stream_player.playing == false:
 		audio_stream_player.play()
-	
-	pass
 
-func _on_area_2d_area_entered(area: Area2D) -> void:
+func _on_area_2d_area_entered(area: Area2D) -> void: #triggers the death method when the victim is hit by the train
 	print("area entered in victim")
 	if area.is_in_group("player"):
 		death()
