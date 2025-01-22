87b312f7b300dcf15c07a99b143ccd96c7ae125e
Drelexal <153316184+Drelexal@users.noreply.github.com>
2025-01-22 18:41:09 +0100

    feat : added a margin shader

--- test/Scripts/scene_transition.gd
+++ test/Scripts/scene_transition.gd
@@ -1,8 +1,8 @@
 extends CanvasLayer
-
+@onready var animation_player = $Control/ColorRect/AnimationPlayer	
 
 func change_scene (target: String) -> void:
 	$Control/ColorRect/AnimationPlayer.play("Transition")
-	await $Control/ColorRect/AnimationPlayer.animation_finished("Transition")
+	await $Control/ColorRect/AnimationPlayer.animation_finished
 	
 	$Control/ColorRect/AnimationPlayer.play_backwards("Transition")
