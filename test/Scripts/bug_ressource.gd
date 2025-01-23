427f5f2fe55774f2e8d3a189eea07279fd46cee8
Drelexal <153316184+Drelexal@users.noreply.github.com>
2025-01-23 18:14:30 +0100

    feat: added a bug icon folder in asset
    feat: added a utility monster sprite/icon

--- test/Scripts/bug_ressource.gd
+++ test/Scripts/bug_ressource.gd
@@ -1,5 +1,8 @@
 extends Resource
 
-@export var bug_Icon: SpriteFrames
+#If there is animated icons later on:
+#@export var bug_anim: SpriteFrames
+
+@export var bug_icon: Texture2D
 @export var Bug_name: String
 @export var bug_description: String
