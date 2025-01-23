482782cc695e9661a952f7e0a898125869d69e41
Drelexal <153316184+Drelexal@users.noreply.github.com>
2025-01-23 01:55:35 +0100

    feat: implemented glitch transition animation and shader

--- test/Scripts/scene_transition.gd
+++ test/Scripts/scene_transition.gd
@@ -1,13 +1,13 @@
 extends CanvasLayer
 
-@onready var color_rect: ColorRect = $Control/ColorRect
-@onready var animation_player: AnimationPlayer = $Control/ColorRect/AnimationPlayer
+@onready var color_rect: ColorRect = $Control/ColorRect_Fade_to_black
+@onready var animation_player: AnimationPlayer = $Control/AnimationPlayer
 
 func fade_in():
-	animation_player.play("Transition")
+	animation_player.play("Transition_Fade_in")
 	await animation_player.animation_finished
-	animation_player.play_backwards("Transition")
+	animation_player.play_backwards("Transition_Fade_in")
 
 func fade_out():
-	animation_player.play_backwards("Transition")
+	animation_player.play_backwards("Transition_Fade_in")
 	await animation_player.animation_finished
