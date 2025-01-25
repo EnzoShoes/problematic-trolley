b76ae0ea675e783f8c26361ed79f83d17dee462a
Eagleast <julesdugon@gmail.com>
2025-01-25 14:03:54 +0100

    fix : cleaning code

--- test/Scripts/game.gd
+++ test/Scripts/game.gd
@@ -15,6 +15,44 @@ func _ready() -> void:
 	score_manager.phase_finished.connect(_on_phase_finished)
 	init_problem(VictimFactory.premade_lvls_map["lvl_" + str(current_lvl)])
 
+func new_problem():
+	problem.queue_free()
+	problem = PROBLEM.instantiate()
+
+	(func():
+		add_child(problem)
+		if Globals.game_state == Globals.game_states.SUPERVISED:
+			init_problem(VictimFactory.premade_lvls_map["lvl_" + str(current_lvl)])
+		elif Globals.game_state == Globals.game_states.UNSUPERVISED:
+			init_problem(VictimFactory.new_random_lvl())
+		elif Globals.game_state == Globals.game_states.END:
+			pass
+	).call_deferred()
+
+func init_problem(lvl):
+	if current_lvl != len(VictimFactory.premade_lvls_map) and Globals.game_state == Globals.game_states.SUPERVISED:
+		current_lvl += 1
+	spawn_victims(lvl)
+	problem.choice_made.connect(_on_choice_made)
+
+func spawn_victims(lvl):
+	print(problem.get_child(2))
+	problem.rails.spawn_victims(lvl)
+
+func free_victims():
+	problem.rails.free_victims(problem.rails.loaded_victims)
+
+func activate_timer():
+	unsupervised_time.start()
+
+func update_timer_ui():
+	ui.update_timer_label(int(unsupervised_time.time_left))
+
+func _on_unsupervised_time_timeout() -> void:
+	score_manager.phase_finished.emit()
+	new_problem()
+	pass # Replace with function body.
+
 func _on_score_updated(score, max_score):
 	var trust_gauge_value = 100 * score / max_score
 	ui.update_trust_bar(trust_gauge_value, 0)
@@ -37,42 +75,3 @@ func _on_choice_made(choice: String):
 		return
 	score_manager.num_choice_made += 1
 	new_problem()
-
-func spawn_victims(lvl):
-	print(problem.get_child(2))
-	problem.rails.spawn_victims(lvl)
-
-func free_victims():
-	problem.rails.free_victims(problem.rails.loaded_victims)
-
-func init_problem(lvl):
-	if current_lvl != len(VictimFactory.premade_lvls_map) and Globals.game_state == Globals.game_states.SUPERVISED:
-		current_lvl += 1
-	spawn_victims(lvl)
-	problem.choice_made.connect(_on_choice_made)
-
-func activate_timer():
-	unsupervised_time.start()
-
-func _on_unsupervised_time_timeout() -> void:
-	score_manager.phase_finished.emit()
-	new_problem()
-	pass # Replace with function body.
-
-func new_problem():
-	problem.queue_free()
-	problem = PROBLEM.instantiate()
-
-	(func():
-		add_child(problem)
-		if Globals.game_state == Globals.game_states.SUPERVISED:
-			init_problem(VictimFactory.premade_lvls_map["lvl_" + str(current_lvl)])
-		elif Globals.game_state == Globals.game_states.UNSUPERVISED:
-			init_problem(VictimFactory.new_random_lvl())
-		elif Globals.game_state == Globals.game_states.END:
-			pass
-	).call_deferred()
-
-func update_timer_ui():
-	ui.update_timer_label(int(unsupervised_time.time_left))
-	
