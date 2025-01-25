12e0ec129c36e733ef953d69bb51fe611dee40de
Eagleast <julesdugon@gmail.com>
2025-01-25 21:56:59 +0100

    fix : added possibility to spawn NONE glitched vicitms

--- test/Scripts/Victims/victim_factory.gd
+++ test/Scripts/Victims/victim_factory.gd
@@ -2,7 +2,6 @@ class_name VictimFactory
 extends Node
 
 const victim_scene: PackedScene = preload("res://Scenes/victims/victim.tscn")
-const ZERO_VALUE_PARTICLES = preload("res://Scenes/particles_systems/zero_value_particles.tscn")
 
 static var victim_resource_map = {
 	Globals.victim_types.BAD : load("res://resources/bad_dude.tres"),
@@ -13,14 +12,19 @@ static var victim_resource_map = {
 }
 
 static var victim_shader_map = {
+	Glitch.glitches.NONE : null,
 	Glitch.glitches.AI_UPLOADING : load("res://resources/Shaders/shader_materials/no_value_victim.tres")
 }
 
-static func new_victim(type: int, glitch: int = Glitch.glitches.AI_UPLOADING) -> Victim:
+static var victim_particles_map = {
+	Glitch.glitches.NONE : preload("res://Scenes/particles_systems/empty_node.tscn"),
+	Glitch.glitches.AI_UPLOADING : load("res://Scenes/particles_systems/zero_value_particles.tscn")
+}
+static func new_victim(type: int, glitch: int = Glitch.glitches.NONE) -> Victim:
 	var victim: Victim = victim_scene.instantiate()
 	victim.ressource = victim_resource_map[type]
 	victim.value = Glitch.victim_value_map[glitch][type]
 	victim.material = victim_shader_map[glitch]
-	var particle_system = ZERO_VALUE_PARTICLES.instantiate()
+	var particle_system = victim_particles_map[glitch].instantiate()
 	victim.get_node("particle_holder").add_child(particle_system)
 	return victim
