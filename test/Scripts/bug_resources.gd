ce0d42f5368360b9632145928b7af1c5136818bd
Drelexal <153316184+Drelexal@users.noreply.github.com>
2025-01-26 19:59:50 +0100

    fix: added back the bug_resources script

---
+++ test/Scripts/bug_resources.gd
@@ -0,0 +1,9 @@
+extends Resource
+
+#If there is animated icons later on:
+#@export var bug_anim: SpriteFrames
+
+@export var bug_icon: Texture2D
+@export var bug_animation: SpriteFrames
+@export var Bug_name: String
+@export var bug_description: String
