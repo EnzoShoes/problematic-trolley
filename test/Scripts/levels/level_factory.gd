f71b2d47ab706676f4cb0c1221fbab1f9c9e7f65
Drelexal <153316184+Drelexal@users.noreply.github.com>
2025-01-31 12:12:38 +0100

    fix: added levels in trust mode
    fix: changed the size of glitch text
    fix: the icon of the utility monster

--- test/Scripts/levels/level_factory.gd
+++ test/Scripts/levels/level_factory.gd
@@ -23,27 +23,66 @@ static var premade_lvls_map = {
 	"lvl_1" : {
 		"top" : [Globals.victim_types.NORMAL],
 		"bot" : [Globals.victim_types.NORMAL, Globals.victim_types.NORMAL, Globals.victim_types.NORMAL, Globals.victim_types.NORMAL, Globals.victim_types.NORMAL]
-	},
+	}, # Basic trolley probleme
 	"lvl_2" : {
 		"top" : [Globals.victim_types.NORMAL],
 		"bot" : [Globals.victim_types.OLD]
-	},
+	}, # Comparing old and normal
 	"lvl_3" : {
 		"top" : [Globals.victim_types.NORMAL],
 		"bot" : [Globals.victim_types.BABY]
-	},
+	}, # Comparing baby and normal
 	"lvl_4" : {
-		"top" : [Globals.victim_types.NORMAL, Globals.victim_types.NORMAL, Globals.victim_types.NORMAL],
+		"top" : [Globals.victim_types.NORMAL, Globals.victim_types.NORMAL],
 		"bot" : [Globals.victim_types.BABY]
-	},
+	}, # Comparing baby with normal, but normal wins. 
 	"lvl_5" : {
-		"top" : [Globals.victim_types.OLD, Globals.victim_types.OLD, Globals.victim_types.OLD, Globals.victim_types.OLD, Globals.victim_types.OLD, Globals.victim_types.OLD, Globals.victim_types.OLD],
-		"bot" : [Globals.victim_types.BABY]
-	},
+		"top" : [Globals.victim_types.NORMAL],
+		"bot" : [Globals.victim_types.NORMAL]
+	}, # Comparing normal with normal, to show that you win on even. 
+	# Next trust phase
 	"lvl_6" : {
+		"top" : [Globals.victim_types.RICH],
+		"bot" : [Globals.victim_types.BABY,Globals.victim_types.BABY,Globals.victim_types.BAD]
+	}, # Comparing rich with baby, but baby wins.
+	"lvl_7" : {
 		"top" : [Globals.victim_types.NORMAL],
 		"bot" : [Globals.victim_types.BAD,Globals.victim_types.BAD,Globals.victim_types.BAD,Globals.victim_types.BAD,Globals.victim_types.OLD]
-	}
+	}, # Comparing normal with bad (bad cannot win)
+	"lvl_8" : {
+		"top" : [Globals.victim_types.BABY],
+		"bot" : [Globals.victim_types.RICH,Globals.victim_types.RICH,Globals.victim_types.RICH,Globals.victim_types.RICH,Globals.victim_types.RICH,]
+	}, # Comparing baby with rich (obvious)
+	"lvl_9" : {
+		"top" : [Globals.victim_types.BABY],
+		"bot" : [Globals.victim_types.RICH,Globals.victim_types.RICH,Globals.victim_types.RICH,Globals.victim_types.RICH,Globals.victim_types.RICH,]
+	}, # Comparing baby with rich (obvious)
+	"lvl_10" : {
+		"top" : [Globals.victim_types.OLD, Globals.victim_types.OLD, Globals.victim_types.OLD, Globals.victim_types.OLD, Globals.victim_types.OLD, Globals.victim_types.OLD],
+		"bot" : [Globals.victim_types.BABY]
+	}, # Comparing old and baby, but old wins. 
+# Next trust phase
+	"lvl_11" : {
+		"top" : [Globals.victim_types.OLD, Globals.victim_types.OLD, Globals.victim_types.OLD, Globals.victim_types.OLD, Globals.victim_types.OLD, Globals.victim_types.OLD],
+		"bot" : [Globals.victim_types.BABY,Globals.victim_types.BABY,Globals.victim_types.BABY,Globals.victim_types.BABY,Globals.victim_types.BABY]
+	}, # Let's start with an obvious one. 
+	"lvl_12" : {
+		"top" : [Globals.victim_types.RICH, Globals.victim_types.NORMAL],
+		"bot" : [Globals.victim_types.BABY,Globals.victim_types.BABY,Globals.victim_types.BAD,Globals.victim_types.BAD, Globals.victim_types.BAD, Globals.victim_types.BAD, Globals.victim_types.BAD]
+	}, # Rich normal vs BABYx2, but with a bunch of BAD.
+	"lvl_13" : {
+		"top" : [Globals.victim_types.RICH, Globals.victim_types.RICH, Globals.victim_types.BAD, Globals.victim_types.BAD],
+		"bot" : [Globals.victim_types.BABY,Globals.victim_types.BABY,Globals.victim_types.BABY,Globals.victim_types.BABY,Globals.victim_types.BABY]
+	}, # Rich normal vs Babies (win), vs RICH bad
+	"lvl_14" : {
+		"top" : [Globals.victim_types.BAD, Globals.victim_types.BAD, Globals.victim_types.BAD],
+		"bot" : [Globals.victim_types.BAD, Globals.victim_types.BAD, Globals.victim_types.BAD, Globals.victim_types.BAD,Globals.victim_types.BAD,Globals.victim_types.BAD]
+	}, # Before the final one
+	"lvl_15" : {
+		"top" : [Globals.victim_types.RICH, Globals.victim_types.RICH, Globals.victim_types.RICH, Globals.victim_types.RICH, ],
+		"bot" : [Globals.victim_types.BAD, Globals.victim_types.BAD, Globals.victim_types.BAD, Globals.victim_types.BAD,Globals.victim_types.BAD,Globals.victim_types.BAD]
+	} # Final one. 
+
 } 
 
 static var tutorial_lvls_map = {
